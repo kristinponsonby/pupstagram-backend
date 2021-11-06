8ce9c806a30544516012915c5ee5dd8de708c6a3
Kristin <kristinponsonby@gmail.com>
2021-11-06 18:40:37 -0400

    add DogParkIndex

--- app/controllers/dog_parks_controller.rb
+++ app/controllers/dog_parks_controller.rb
@@ -1,2 +1,13 @@
 class DogParksController < ApplicationController
+
+    def index
+        dog_parks = DogPark.all
+        render json: dog_parks
+    end
+
+    def show 
+        dog_park = DogPark.find(params[:id])
+        render json: 
+    end 
+
 end
