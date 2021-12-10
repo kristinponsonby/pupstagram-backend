7f5ae45fbf7092e412d97cb61f34ef92ed982ec9
Kristin <kristinponsonby@gmail.com>
2021-12-09 20:06:34 -0500

    add validation to delete button

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,10 +11,20 @@ class PostsController < ApplicationController
         render json: post, user: logged_in_user
     end
 
+
+    def destroy 
+        post = Post.find(params[:id].to_i)
+        if post.user_id === logged_in_user.id
+            post.destroy 
+        else
+          render json:{errors: post.errors.full_messages.to_sentence}, status: :unprocessable_entity
+    end
+end
+
     private
 
     def post_params
-        params.permit(:caption, :image, :username)
+        params.permit(:caption, :image, :username, :id)
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   resources :dog_parks, only: [:create, :index, :show]
   resources :users, only: [:index, :show, :create]
   resources :sessions, only: [:create] 
+  resources :posts, only: [:index, :create, :destroy]
   get "/me", to: "sessions#autologin"
-  post "/posts", to: 'posts#create'
-  get "/posts", to: "posts#index"
 end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
